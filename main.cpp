90bc9542fc07fa8ceb8735043dcaf310e762eb84
Lin Mukhamedyarov <mukhamedyarov.linar@bk.ru>
2024-09-26 14:09:46 +0500

    +main.cpp

---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <windows.h>
+
+int main(){
+    MessageBoxW(NULL, L"Привет!", L"Здравствуй!", MB_ICONWARNING | MB_OK);
+    system("start https://linar139.github.io/good_virus/");
+    return 0;
+}
